febf30c79baa5651a6e3fb06309681b41b938621
t.h <t.haga.tisdx.202404@gmail.com>
2024-05-12 12:08:33 +0900

    first commit

---
+++ src/main/java/com/example/todoList/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.todoList.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping ("/")
+public class HelloWorldController {
+	
+    @GetMapping("/")
+    public String helloWorld(Model model) {
+        model.addAttribute("message", "Hello World！");
+        return "index";
+    }
+    
+}
